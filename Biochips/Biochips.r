d2d8e7b100e7813559918ab4c4e322916c1b720e
Johnson <scfengprogramming@gmail.com>
2019-06-12 21:14:18 +0800

    add function calculate_distance_between_two_assignees. add function detect_cycle.

--- Biochips/Biochips.r
+++ Biochips/Biochips.r
@@ -1,14 +1,16 @@
 #Distance between two sets.
-#Version:0.1.0
-#Last Update:6/5/2019
+#Version:0.1.1
+#Last Update:6/12/2019
 
 #
 #
 #Packages required: igraph.
 #
-#Initialization commands:
-#library("igraph")
-#source(file = file.choose())
+#Initialization steps in R:
+#1. library("igraph")
+#2. source(file = file.choose()) (e.g. Biochips.r)
+#3. select pajek network file (e.g. Biochips_Cited_Citing.net)
+#4. select assignee file (e.g. Biochips_Assignee_Pajek.csv)
 #
 #
 
@@ -16,7 +18,7 @@
 library("igraph")
 
 #initialize the network
-g <- read.graph(file.choose(), format = "pajek")
+graph <- read.graph(file.choose(), format = "pajek")
 
 #dat <- read.csv(file.choose())
 #m <- as.matrix(dat)
@@ -25,14 +27,17 @@ g <- read.graph(file.choose(), format = "pajek")
 #w <- E(net)$weight
 #td <- 0
 
+#initialize assignee
+assignee <- read.csv(file.choose())
+
 #initialize set A
-a <- read.csv(file.choose())
+#a <- read.csv(file.choose())
 
 #initialize set B
-b <- read.csv(file.choose())
+#b <- read.csv(file.choose())
 
 #Calculate a total distance between two vectors.
-distance_between_two_sets <- function(setA, setB) {
+calculate_distance_between_two_sets <- function(setA, setB) {
   td <- 0
   r <- matrix(nrow = lengths(setA) * lengths(setB), ncol = 3)
   
@@ -41,7 +46,7 @@ distance_between_two_sets <- function(setA, setB) {
 
     for(j in 1:lengths(setB)) {
       #print(j)
-      temp <- get.shortest.paths(g, setA[[1]][i], setB[[1]][j])
+      temp <- get.shortest.paths(graph, setA[[1]][i], setB[[1]][j])
       td <- td + lengths(temp[[1]]) - 1
       #print(td)
       
@@ -55,3 +60,26 @@ distance_between_two_sets <- function(setA, setB) {
   paste("The total between setA and setB is: ", td, sep="")
 }
 
+#calculate distance between two assignees
+calculate_distance_between_two_assignees <- function(assigneeA, assigneeB) {
+  set_a <- which(assignee == assigneeA, arr.ind = TRUE)
+  set_a <- set_a[, -2]
+  set_a <<- as.data.frame(assignee[set_a, 3])
+  set_b <- which(assignee == assigneeB, arr.ind = TRUE)
+  set_b <- set_b[, -2]
+  set_b <<- as.data.frame(assignee[set_b, 3])
+  #calculate_distance_between_two_sets(set_a, set_b)
+}
+
+#detect wheather exist cycles in a graph
+detect_cycle <- function(g) {
+  cy <- NULL
+  
+  for(v1 in V(g)) {
+    for(v2 in neighbors(g, v1, mode="out")) {
+      cy <- c(cy, lapply(all_simple_paths(g, v2,v1, mode="out"), function(p) c(v1,p)))
+    }
+  }
+  
+  cycles <<- cy
+}
